7b1a53bbe87cc5d212e811ab8c384ec8a4e329f9
GustavoLGregorio <163776400+GustavoLGregorio@users.noreply.github.com>
2025-12-09 22:30:51 -0300

    Update files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
-import React from "react";
-import Header from "@/components/Header";
+import Header from "@/components/organisms/Header";
+import TitleRedirect from "@/components/molecules/TitleRedirect";
+import Section from "@/components/templates/Section";
+import CardContainerHorizontal from "@/components/organisms/CardContainerHorizontal";
+import Carousel from "@/components/organisms/Carousel";
 
 export default function HomePage() {
   return (
     <>
       <Header />
+      <Carousel></Carousel>
+      <Section>
+        <TitleRedirect title="Recomendados" redirectTo={"https://google.com"} />
+        <CardContainerHorizontal />
+      </Section>
+      <Section>
+        <TitleRedirect title="Recomendados" redirectTo={"https://google.com"} />
+        <CardContainerHorizontal />
+      </Section>
+      <Section>
+        <TitleRedirect title="Recomendados" redirectTo={"https://google.com"} />
+        <CardContainerHorizontal />
+      </Section>
     </>
   );
 }
